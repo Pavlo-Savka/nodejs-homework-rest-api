9e2da96161e1b871f556b2a5ce1050000025ab72
Pavlo-Savka <savka.pavlo@gmail.com>
2023-07-16 16:44:05 +0300

    fix

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -8,12 +8,12 @@ const authenticate = async (req, res, next) => {
     try {
     const { authorization = "" } = req.headers
     const [bearer, token] = authorization.split(" ");
-    if (bearer !== "Bearer") {
+    if (bearer !== "Bearer" || !token ) {
         next(HttpError(401, "No token"))
     }
         const { id } = jwt.verify(token, SECRET_KEY)
         const user = await User.findById(id)
-        if (!user || !user.token || user.token !== token)
+        if (!user || !user.token || user.token !== token)   
         {
              next(HttpError(401, "No user"))
         }
