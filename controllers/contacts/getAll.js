82152ec8a8e27b02c64cb994d8148e1a1fca2776
Pavlo-Savka <savka.pavlo@gmail.com>
2023-07-18 23:28:32 +0300

    fix

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -4,7 +4,6 @@ const { Contact } = require('../../models/contact')
 
 const getAll = async (req, res, next) => {
   try {
-    console.log(req.user)
     const { _id: owner } = req.user
     const { page = 1, limit = 10 } = req.query
     const skip = (page - 1) * limit;
