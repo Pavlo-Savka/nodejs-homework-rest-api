0ce01fbda9c8a838138cfe8862746828028389e4
Pavlo-Savka <savka.pavlo@gmail.com>
2023-07-21 01:38:04 +0300

    nodemailer

---
+++ helpers/sendEmail.js
@@ -0,0 +1,31 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config;
+
+const { META_PASSWORD } = process.env;
+
+const nodemailerConfig = {
+    host: "smtp.meta.ua",
+    port: 465,
+    secure: true,
+    auth: {
+        user: "savka.pavlo@meta.ua",
+        pass: META_PASSWORD
+    }
+}
+const transport = noodemailer.createTransport(nodemailerConfig);
+
+// const data = {
+//     to: "savka.pavlo@gmail.com",
+//     subject: "test message",
+//     html: "<p><strong>Test Email</strong> from loocalhost 3000</p>"
+// }
+
+const sendEmail = async (data) => {
+    const email = { ...data, from: "savka.pavlo@meta.ua" };
+    await transport.sendMail(email)
+    .then(() => console.log("Email send success"))
+        .catch(error => console.log(error.message));
+    return true
+}
+
+module.exports = sendEmail;
